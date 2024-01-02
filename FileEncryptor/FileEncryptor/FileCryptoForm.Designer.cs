12a3b58a044c579762419122d4f9b2d364191274
Dylan Dotti <invocation42@yahoo.com>
2024-01-02 02:00:11 -0500

    Enclose file/folder selection within "target selection" wrapper

--- FileEncryptor/FileEncryptor/FileCryptoForm.Designer.cs
+++ FileEncryptor/FileEncryptor/FileCryptoForm.Designer.cs
@@ -30,11 +30,10 @@ namespace FileEncryptor
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
-            this.selectButton = new System.Windows.Forms.Button();
-            this.selectionLabel = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.selectionLabel = new System.Windows.Forms.Label();
+            this.selectButton = new System.Windows.Forms.Button();
             this.fileRadioButton = new System.Windows.Forms.RadioButton();
             this.folderRadioButton = new System.Windows.Forms.RadioButton();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
@@ -46,7 +45,6 @@ namespace FileEncryptor
             this.fileBrowser = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
             this.tableLayoutPanel1.SuspendLayout();
-            this.tableLayoutPanel4.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
@@ -57,60 +55,18 @@ namespace FileEncryptor
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel4, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel5, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel5, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(403, 283);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(425, 312);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // tableLayoutPanel4
-            // 
-            this.tableLayoutPanel4.ColumnCount = 2;
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
-            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel4.Controls.Add(this.selectButton, 0, 0);
-            this.tableLayoutPanel4.Controls.Add(this.selectionLabel, 1, 0);
-            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 88);
-            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
-            this.tableLayoutPanel4.RowCount = 1;
-            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel4.Size = new System.Drawing.Size(397, 72);
-            this.tableLayoutPanel4.TabIndex = 2;
-            // 
-            // selectButton
-            // 
-            this.selectButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.selectButton.Location = new System.Drawing.Point(30, 16);
-            this.selectButton.Margin = new System.Windows.Forms.Padding(30, 10, 10, 10);
-            this.selectButton.Name = "selectButton";
-            this.selectButton.Size = new System.Drawing.Size(158, 39);
-            this.selectButton.TabIndex = 0;
-            this.selectButton.Text = "Select File";
-            this.selectButton.UseVisualStyleBackColor = true;
-            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
-            // 
-            // selectionLabel
-            // 
-            this.selectionLabel.AutoSize = true;
-            this.selectionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.selectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectionLabel.Location = new System.Drawing.Point(208, 10);
-            this.selectionLabel.Margin = new System.Windows.Forms.Padding(10);
-            this.selectionLabel.Name = "selectionLabel";
-            this.selectionLabel.Size = new System.Drawing.Size(179, 52);
-            this.selectionLabel.TabIndex = 1;
-            this.selectionLabel.Text = "No file selected";
-            this.selectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // groupBox1
             // 
@@ -121,7 +77,7 @@ namespace FileEncryptor
             this.groupBox1.Location = new System.Drawing.Point(10, 10);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(10);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(383, 65);
+            this.groupBox1.Size = new System.Drawing.Size(405, 153);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Target Selection";
@@ -131,17 +87,45 @@ namespace FileEncryptor
             this.tableLayoutPanel2.AutoSize = true;
             this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel2.ColumnCount = 2;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.selectionLabel, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.selectButton, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.fileRadioButton, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.folderRadioButton, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 18);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(377, 44);
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(399, 132);
             this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // selectionLabel
+            // 
+            this.selectionLabel.AutoEllipsis = true;
+            this.selectionLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.selectionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectionLabel.Location = new System.Drawing.Point(202, 76);
+            this.selectionLabel.Margin = new System.Windows.Forms.Padding(10);
+            this.selectionLabel.Name = "selectionLabel";
+            this.selectionLabel.Size = new System.Drawing.Size(187, 46);
+            this.selectionLabel.TabIndex = 2;
+            this.selectionLabel.Text = "No file selected";
+            this.selectionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // selectButton
+            // 
+            this.selectButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.selectButton.Location = new System.Drawing.Point(30, 76);
+            this.selectButton.Margin = new System.Windows.Forms.Padding(30, 10, 10, 10);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(152, 46);
+            this.selectButton.TabIndex = 0;
+            this.selectButton.Text = "Select File";
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
             // 
             // fileRadioButton
             // 
@@ -149,7 +133,7 @@ namespace FileEncryptor
             this.fileRadioButton.AutoSize = true;
             this.fileRadioButton.Checked = true;
             this.fileRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.fileRadioButton.Location = new System.Drawing.Point(76, 10);
+            this.fileRadioButton.Location = new System.Drawing.Point(78, 21);
             this.fileRadioButton.Margin = new System.Windows.Forms.Padding(20, 10, 3, 10);
             this.fileRadioButton.Name = "fileRadioButton";
             this.fileRadioButton.Size = new System.Drawing.Size(52, 24);
@@ -161,10 +145,10 @@ namespace FileEncryptor
             // 
             // folderRadioButton
             // 
-            this.folderRadioButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.folderRadioButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.folderRadioButton.AutoSize = true;
             this.folderRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.folderRadioButton.Location = new System.Drawing.Point(255, 10);
+            this.folderRadioButton.Location = new System.Drawing.Point(212, 21);
             this.folderRadioButton.Margin = new System.Windows.Forms.Padding(20, 10, 3, 10);
             this.folderRadioButton.Name = "folderRadioButton";
             this.folderRadioButton.Size = new System.Drawing.Size(72, 24);
@@ -181,18 +165,18 @@ namespace FileEncryptor
             this.tableLayoutPanel3.Controls.Add(this.decryptButton, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.encryptButton, 0, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 207);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 217);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(397, 73);
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(419, 92);
             this.tableLayoutPanel3.TabIndex = 1;
             // 
             // decryptButton
             // 
             this.decryptButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.decryptButton.Enabled = false;
-            this.decryptButton.Location = new System.Drawing.Point(208, 17);
+            this.decryptButton.Location = new System.Drawing.Point(219, 26);
             this.decryptButton.Margin = new System.Windows.Forms.Padding(10);
             this.decryptButton.Name = "decryptButton";
             this.decryptButton.Size = new System.Drawing.Size(158, 39);
@@ -205,7 +189,7 @@ namespace FileEncryptor
             // 
             this.encryptButton.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.encryptButton.Enabled = false;
-            this.encryptButton.Location = new System.Drawing.Point(30, 17);
+            this.encryptButton.Location = new System.Drawing.Point(41, 26);
             this.encryptButton.Margin = new System.Windows.Forms.Padding(10);
             this.encryptButton.Name = "encryptButton";
             this.encryptButton.Size = new System.Drawing.Size(158, 39);
@@ -223,18 +207,18 @@ namespace FileEncryptor
             this.tableLayoutPanel5.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel5.Controls.Add(this.passwordInput, 1, 0);
             this.tableLayoutPanel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel5.Location = new System.Drawing.Point(3, 166);
+            this.tableLayoutPanel5.Location = new System.Drawing.Point(3, 176);
             this.tableLayoutPanel5.Name = "tableLayoutPanel5";
             this.tableLayoutPanel5.RowCount = 1;
             this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel5.Size = new System.Drawing.Size(397, 35);
+            this.tableLayoutPanel5.Size = new System.Drawing.Size(419, 35);
             this.tableLayoutPanel5.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 7);
+            this.label1.Location = new System.Drawing.Point(44, 7);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 0;
@@ -244,7 +228,7 @@ namespace FileEncryptor
             // 
             this.passwordInput.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.passwordInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordInput.Location = new System.Drawing.Point(122, 3);
+            this.passwordInput.Location = new System.Drawing.Point(128, 3);
             this.passwordInput.Name = "passwordInput";
             this.passwordInput.Size = new System.Drawing.Size(191, 29);
             this.passwordInput.TabIndex = 1;
@@ -254,7 +238,7 @@ namespace FileEncryptor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(403, 283);
+            this.ClientSize = new System.Drawing.Size(425, 312);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -262,8 +246,6 @@ namespace FileEncryptor
             this.Text = "File Encryption";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            this.tableLayoutPanel4.ResumeLayout(false);
-            this.tableLayoutPanel4.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.tableLayoutPanel2.ResumeLayout(false);
@@ -278,14 +260,12 @@ namespace FileEncryptor
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
         private System.Windows.Forms.Button selectButton;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
         private System.Windows.Forms.RadioButton fileRadioButton;
         private System.Windows.Forms.RadioButton folderRadioButton;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.Label selectionLabel;
         private System.Windows.Forms.Button decryptButton;
         private System.Windows.Forms.Button encryptButton;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
@@ -293,5 +273,6 @@ namespace FileEncryptor
         private System.Windows.Forms.TextBox passwordInput;
         private System.Windows.Forms.OpenFileDialog fileBrowser;
         private System.Windows.Forms.FolderBrowserDialog folderBrowser;
+        private System.Windows.Forms.Label selectionLabel;
     }
 }
